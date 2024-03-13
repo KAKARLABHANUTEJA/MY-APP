f1ef011755ddd0db39a77167044ac019570ed8e8
Bhanu Teja <bhanuteja8339@gmail.com>
2024-03-13 15:01:44 -0400

    propos and composition

--- README.md
+++ README.md
@@ -4,10 +4,13 @@ The UI should be comfortable to users who share room and spaces with friends.
 
 The inital idea is to implement the following features: A webpage to track the expenses, a webpage to track the room chores divided by the room mates
 
+Deployment: March 13
+This deployment on 13 March includes the learnings of props and composition(pass common styles from parent to children components)
+
 deploy to github :
 install ghpages in local: npm install gh-pages --save-dev
 
-link the repository to your local repo: git remote add origin https://.............git
+link the repository to your local repo: git remote add origin https://.............git  (https://github.com/KAKARLABHANUTEJA/MY-APP.git)
 
 modify package.json: add homepage before dependencies ("homepage": "http://username.github.io/repositoryName")
                      update scripts: add predeploy ("predeploy": "npm run build"
--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,4 +1,7 @@
 import './ExpenseItems.css';
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
+
 function ExpenseItems(props){
     const expenseDate = new Date();
     //const expenseDate = new Date(2021,2,28) - to set a specific date;
@@ -8,35 +11,13 @@ function ExpenseItems(props){
     const day = props.date.toLocaleString('en-US', {day:'2-digit'});
     const year = props.date.getFullYear();
     return (
-        //<h2>Expense Item!!</h2>
-        // <div className="expense-item">
-        //     <div>July 19, 2023</div>
-        //     <div>
-        //         <h2>Mobile Insurance</h2>
-        //         <div>$65.99</div>
-        //     </div>
-        // </div>
-        //before using props
-        // <div className="expense-item">
-        //     <div>{expenseDate.toISOString()}</div>
-        //     <div className="expense-item__description">
-        //         <h2>{expenseTitle}</h2>
-        //         <div className="expense-item__price">${expenseAmount}</div>
-        //     </div>
-        // </div>
-
-        //after using props
-        <div className="expense-item">
-            <div>
-                <div>{month}</div>
-                <div>{day}</div>
-                <div>{year}</div>
-            </div>
+        <Card className="expense-item">
+            <ExpenseDate date={props.date}></ExpenseDate>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
-        </div>
+        </Card>
     );
 }
 
